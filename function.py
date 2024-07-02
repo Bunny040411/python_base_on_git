50dc2f7bcdfdc716d37f4b6491fe79cdc158be25
unknown <2036217354@qq.com>
2024-07-02 20:57:16 +0800

    24-7-2-1st

---
+++ function.py
@@ -0,0 +1,7 @@
+import numpy
+
+# conda list 显示安装包
+# pip list 
+# conda env list 显示环境目录
+def func1(a):
+    return a+1
---
+++ run.py
@@ -0,0 +1,5 @@
+from function import *
+# 从function文件中引用所有函数和类
+
+num = int(input())
+print(func1(num))
